0a5ff8db4ec5e8f6eef92bef7954172503c3748a
aadenl2009 <aadenlam@connect.kellettschool.com>
2025-04-01 03:49:12 +0000

    Apr 1, 2025, 3:49 AM

--- main.py
+++ main.py
@@ -36,17 +36,25 @@ def draw_two_cards(x, y):
             y += num
             x.append(num)
 
-def first_two_cards():
+def game_start():
     bet = input(f"You currently have {user_money}. Please place your bet:")
-    
-    # draw the cards
-    draw_two_cards(user_hand, user_score)
-    print(f"Your hand: {user_hand}")
 
-    draw_two_cards(dealer_hand, dealer_score)
-    print(f"Dealer's hand: {dealer_hand[0]}, x")
+    if user_score == 21:
+        print("BlackJack! You Win!")
+        user_money += bet * 1.5
+        play_again = input((f"You now have {user_money}. Play again? (y/n)"))
+
+        if play_again == "y":
+            game_start()
+
+    else:
+        draw_two_cards(user_hand, user_score)
+        print(f"Your hand: {user_hand}")
+
+        draw_two_cards(dealer_hand, dealer_score)
+        print(f"Dealer's hand: {dealer_hand[0]}, x")
 
 def draw_cards():
     hit_stand = input("Hit or stand? (y/n)")
 
-first_two_cards()
+game_start()
