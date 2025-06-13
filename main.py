663f9792f37b4b35a1105fe08a93f7d46cf40847
aadenl2009 <aadenlam@connect.kellettschool.com>
2025-06-13 01:30:52 +0000

    Jun 13, 2025, 1:30 AM

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def game_start(bet, user_money, user_hand, user_score, dealer_hand, dealer_score
     
     user_hand, user_score = hit_stand(user_hand, user_score)
 
-def dealer_show(dealer_score, user_money, dealer_hand, user_hand, user_hand_2, bet, hand_2):
+def dealer_show(dealer_score, user_money, dealer_hand, user_hand, user_hand_2, bet, hand_2, user_score):
 
     user_hand, user_hand_2 = split(user_hand, user_score, hand_2, user_hand_2, user_score_2, bet, user_money)
 
@@ -134,7 +134,7 @@ def hit_stand(hand, score):
 
         hit_stand = input("Hit or stand?").lower().strip()
 
-    dealer_show(dealer_score, user_money, dealer_hand, user_hand, user_hand_2, bet, hand_2)
+    dealer_show(dealer_score, user_money, hand, hand, user_hand_2, bet, hand_2, score)
 
     return hand, score
 
@@ -161,7 +161,7 @@ def double_down(user_hand, user_score, bet, user_money):
         bet *= 2
         user_hand = draw_card(user_hand)
         user_score = calculate_score(user_hand)
-        dealer_show(dealer_score, user_money, dealer_hand, user_hand, user_hand_2, bet, hand_2)
+        dealer_show(dealer_score, user_money, dealer_hand, user_hand, user_hand_2, bet, hand_2, user_score)
 
     return user_hand, user_score
 
