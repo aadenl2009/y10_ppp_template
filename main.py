06a1790893080b39f2de24356f43a9130d16d97d
aadenl2009 <aadenlam@connect.kellettschool.com>
2025-06-13 01:37:48 +0000

    Jun 13, 2025, 1:37 AM

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def hit_stand(hand, score, hand_2):
 
     dealer_show(dealer_score, user_money, dealer_hand, hand, user_hand_2, bet, hand_2, user_score)
     if hand_2 == True:
-        user_hand, user_hand_2 = split(user_hand, user_score, hand_2, user_hand_2, user_score_2, bet, user_money)
+        hand = split(user_hand, user_score, hand_2, user_hand_2, user_score_2, bet, user_money)
     return hand, score
 
 def play_again(user_money):
